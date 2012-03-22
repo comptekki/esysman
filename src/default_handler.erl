91baf4161311e112e69dcfc948f6ee701a50fb93
comptekki <comptekki@gmail.com>
2012-03-22 13:28:28 -0600

    initial commit

---
+++ src/default_handler.erl
@@ -0,0 +1,43 @@
+%% Copyright (c) 2012, Wes James <comptekki@gmail.com>
+%% All rights reserved.
+%% 
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%% 
+%%     * Redistributions of source code must retain the above copyright
+%%       notice, this list of conditions and the following disclaimer.
+%%     * Redistributions in binary form must reproduce the above copyright
+%%       notice, this list of conditions and the following disclaimer in the
+%%       documentation and/or other materials provided with the distribution.
+%%     * Neither the name of "ESysMan" nor the names of its contributors may be
+%%       used to endorse or promote products derived from this software without
+%%       specific prior written permission.
+%% 
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%% POSSIBILITY OF SUCH DAMAGE.
+%% 
+%%
+
+-module(default_handler).
+-behaviour(cowboy_http_handler).
+-export([init/3, handle/2, terminate/2]).
+
+init({_Any, http}, Req, []) ->
+	{ok, Req, undefined}.
+
+handle(Req, State) ->
+	{ok,Req2}=cowboy_http_req:set_resp_header('Location',<<"/esysman">>,Req),
+	{ok, Req3} = cowboy_http_req:reply(307, [],<<>>, Req2),
+	{ok, Req3, State}.
+
+terminate(_Req, _State) ->
+	ok.
