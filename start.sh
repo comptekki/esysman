15ce63e789953c7f6087395dc9fccf3286f27395
comptekki <comptekki@gmail.com>
2012-10-26 12:18:17 -0600

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-erl -name ecom2@host -pa ebin -pa deps/*/ebin -s esysman \
+erl -name node@host -pa ebin -pa deps/*/ebin -s esysman -setcookie some-cookie\
 	-eval "io:format(\"~n~nThe following examples are available:~n\")." \
 	-eval "io:format(\"* Hello world: http://localhost:8080~n\")." \
 	-eval "io:format(\"* Websockets: http://localhost:8080/websocket~n\")." \
