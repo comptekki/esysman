343dd8467adcc2b028bee546d095ad3f18a83264
comptekki <comptekki@gmail.com>
2013-01-11 13:44:42 -0700

    update for cowboy master

--- src/esysman_sup.erl
+++ src/esysman_sup.erl
@@ -33,15 +33,14 @@
 -export([start_link/0]). %% API.
 -export([init/1]). %% supervisor.
 
--define(SUPERVISOR, ?MODULE).
-
 %% API.
 
--spec start_link() -> {ok, Pid::pid()}.
+-spec start_link() -> {ok, pid()}.
 start_link() ->
-	supervisor:start_link({local, ?SUPERVISOR}, ?MODULE, []).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% supervisor.
 
 init([]) ->
-	{ok, {{one_for_one, 10, 10}, []}}.
+	Procs = [],
+	{ok, {{one_for_one, 10, 10}, Procs}}.
