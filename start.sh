4331bd1896f520f098822aafc1410c5a152f7e32
comptekki <comptekki@gmail.com>
2013-01-11 13:24:52 -0700

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 erl -name node@host -pa ebin -pa deps/*/ebin -s esysman -setcookie some-cookie\
-	-eval "io:format(\"~n~nThe following examples are available:~n\")." \
-	-eval "io:format(\"* Hello world: http://localhost:8080~n\")." \
-	-eval "io:format(\"* Websockets: http://localhost:8080/websocket~n\")." \
-    -eval "io:format(\"* Hello World (ssl): https://localhost:8443~n\")." \
-    -eval "io:format(\"* Websockets (ssl): https://localhost:8443/websocket~n\")."
+	-eval "io:format(\"~n~nThe following sites are available:~n\")." \
+	-eval "io:format(\"* http://localhost:8080/esysman~n\")." \
+    -eval "io:format(\"* https://localhost:8443/esysman~n\")."
