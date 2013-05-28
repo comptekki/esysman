73ae518f312a159f3dc3ed25ea6bb3a9b5e30721
comptekki <comptekki@gmail.com>
2013-05-28 09:19:31 -0600

    fix

--- start-ssl.sh
+++ start-ssl.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-erl -boot /path/to/esysman_ssl-16b -proto_dist inet_tls \
-	-ssl_dist_opt server_certfile "/path/to/ssl/cert.pem" \
-	-ssl_dist_opt server_keyfile "/path/to/ssl/key.pem" \
+erl -boot /usr/local/src/esysman/esysman_ssl -proto_dist inet_tls \
+	-ssl_dist_opt server_certfile "/usr/local/src/esysman/priv/ssl/cert.pem" \
+	-ssl_dist_opt server_keyfile "/usr/local/src/esysman/priv/ssl/key.pem" \
 	-ssl_dist_opt server_secure_renegotiate true client_secure_renegotiate true \
 	-name node@host -pa ebin -pa deps/*/ebin -s esysman \
 	-eval "io:format(\"~n~nThe following examples are available:~n\")." \
