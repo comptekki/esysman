081dbf0286f0759c6e8bc377fa1a16189fa84a1d
comptekki <comptekki@gmail.com>
2013-11-21 16:47:37 -0700

    move to release

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-erl -name node@host -pa ebin -pa deps/*/ebin -s esysman -setcookie some-cookie\
-	-eval "io:format(\"~n~nThe following sites are available:~n\")." \
-	-eval "io:format(\"* http://localhost:8080/esysman~n\")." \
-    -eval "io:format(\"* https://localhost:8443/esysman~n\")."
