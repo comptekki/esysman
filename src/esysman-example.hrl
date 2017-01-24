adb8918d8ac4121c73a84fbfa97714ebf05af2ce
comptekki <comptekki@gmail.com>
2017-01-24 13:58:12 -0700

    update for new features

--- src/esysman-example.hrl
+++ src/esysman-example.hrl
@@ -1,13 +1,30 @@
 -define(BROADCAST_ADDR, {192,168,0,255}).
 -define(APPS, [<<"">>,<<"any.cmd">>,<<"any.exe">>,<<"any.msi">>,<<"any.msp">>,<<"any.reg">>,<<"ecom.beam">>,<<"NiniteOne.exe">>,<<"ninite.cmd">>,<<"WUInstall.exe">>]).
--define(COMS, [<<"">>,<<"anycmd">>,<<"listupfls">>,<<"mkuploads">>,<<"ninite">>,<<"ninitecmd">>,<<"ninitelog">>,<<"wuinstall">>,<<"wuilog">>,<<"wolbnr360">>,<<"unamea">>,<<"lsbver">>,<<"yumcheck">>,<<"yumupdate">>,<<"ubuntuver">>,<<"aptcheck">>,<<"aptupgrade-list">>,<<"aptupgrade">>,<<"aptdistupgrade-list">>,<<"aptdistupgrade">>,<<"aptulog">>,<<"osxsupdate">>,<<"osxsulog">>]).
+-define(COMS, [<<"">>,<<"anycmd">>,<<"listupfls">>,<<"mkuploads">>,<<"ninite">>,<<"ninitecmd">>,<<"ninitelog">>,<<"wuinstall">>,<<"wuilog">>,<<"unamea">>,<<"lsbver">>,<<"yumcheck">>,<<"yumupdate">>,<<"ubuntuver">>,<<"aptcheck">>,<<"aptupgrade-list">>,<<"aptupgrade">>,<<"aptdistupgrade-list">>,<<"aptdistupgrade">>,<<"aptulog">>,<<"osxsupdate">>,<<"osxsulog">>]).
 -define(CONF,"/usr/local/src/esysman/src/esysman.conf").
 -define(UPLOADS,<<"/usr/local/src/uploads/">>).
+
+-define(IGNORESHUTDOWN,"box1:box2:box3").
+-define(IGNOREDUPES,"box1").
+-define(IGNOREREBOOT,"box1").
+-define(IGNORESHOWUSERS,"box1").
+-define(IGNOREUSERS,"u1:Default2:default2").
+-define(IGNOREUSERS2,<<"u1:Default2:default2">>).
+-define(IGNOREU1,"u1").
+-define(IGNOREU2,"u2").
+-define(IGNOREU3,"u3").
+-define(IGNOREU4,"u4").
+
+-define(SHUTDOWNSTART,"22").
+-define(SHUTDOWNEND,"06").
+-define(SELECTEDON,"selected").
+-define(SELECTEDOFF," ").
+
 -define(MAXAGE, 500).
 -define(TITLE, "ESysMan").
 -define(CSS, "/static/esysman.css").
--define(JQUERY, "/static/jquery-1.9.1.min.js").
--define(LINES, "15000").
+-define(JQUERY, "/static/jquery-2.1.1.min.js").
+-define(LINES, "10000").
 -define(ROOMS,
 [
  [<<"room1">>,
