481e3a4b7ddff4d0c039046878126bf7b612eef7
comptekki <comptekki@gmail.com>
2017-02-15 11:12:01 -0700

    don't log script content and info

--- src/websocket_handler.erl
+++ src/websocket_handler.erl
@@ -204,8 +204,8 @@ websocket_handle({text, Msg}, Req, State) ->
 								 <<"">>
 				end,
 
-				io:format("~n done edit script file: ~p -> info: ~p~n",[Dataf,Datafi]),
-				Data2= <<"done edit script file...:editscrfile:">>, %,Dataf/binary,":",Datafi/binary>>,
+				io:format("~n done edit script file: ...~n"),
+				Data2= <<"done edit script file...:editscrfile:",Dataf/binary,":",Datafi/binary>>,
 				Data2;
 			<<"savescrfile">> ->
 				[Fname,Dataf,Datafi] = binary:split(Args, <<"+">>, [global]),
