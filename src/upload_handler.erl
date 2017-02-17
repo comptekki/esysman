37bc30ebf8234813e47eb8ae830e7d5434807253
comptekki <comptekki@gmail.com>
2017-02-17 14:16:06 -0700

    fix for large files

--- src/upload_handler.erl
+++ src/upload_handler.erl
@@ -1,7 +1,6 @@
 %% @doc Upload handler.
 %% https://github.com/ninenines/cowboy/ 1.1.2 /examples/upload/src/upload_handler.erl
 -module(upload_handler).
--behaviour(cowboy_http_handler).
 
 -export([init/3]).
 -export([handle/2]).
@@ -14,20 +13,40 @@ init(_, Req, _Opts) ->
 
 handle(Req, State) ->
 	{ok, Headers, Req2} = cowboy_req:part(Req),
-	{ok, Data, Req3} = cowboy_req:part_body(Req2),
 	{file, <<"inputfile">>, Filename, ContentType, _TE}
 		= cow_multipart:form_data(Headers),
-%	io:format("Received file ~p of content-type ~p as follow:~n~p~n~n", [Filename, ContentType, Data]),
-	io:format("Received file ~p of content-type ~p~n~n", [Filename, ContentType]),
-	case file:write_file(<<(?UPLOADS)/binary,Filename/binary>>, Data) of
-		ok ->
-			Res = <<"ok">>,
-			Res;
-		{error, Res} ->
-			Res
+	case file:delete(<<(?UPLOADS)/binary,Filename/binary>>) of
+		ok -> "";
+		{error, _} -> ""
 	end,
-
+	{ok, Req3} = body_to_console(Req2, Filename), 
+	io:format("Received file ~p of content-type ~p~n~n", [Filename, ContentType]),
 	{ok, Req3, State}.
+
+body_to_console(Req, Filename) ->
+    case cowboy_req:part_body(Req) of
+        {ok, Data, Req2} ->
+			case file:write_file(<<(?UPLOADS)/binary,Filename/binary>>, Data, [append]) of
+				ok ->
+					Res = <<"ok">>,
+					Res;
+				{error, Res} ->
+					Res
+			end,
+			{ok, Req2};
+		{more, Data, Req2} ->
+			case file:write_file(<<(?UPLOADS)/binary,Filename/binary>>, Data, [append]) of
+				ok ->
+					Res = <<"ok">>,
+					Res;
+				{error, Res} ->
+					Res
+			end,
+            body_to_console(Req2, Filename),
+            {ok, Req2}
+    end.
 
 terminate(_Reason, _Req, _State) ->
 	ok.
+
+
