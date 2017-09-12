14e623fe4e48038ab6db321df481bbff623fbb83
comptekki <comptekki@gmail.com>
2017-09-12 13:48:37 -0600

    Update cert script and settings

--- mkcert.bash
+++ mkcert.bash
@@ -1,21 +1,20 @@
 #!/bin/bash
-if [ -a ./priv/ssl/cert.pem -o -a ./priv/ssl/key.pem ]
+if [ -a ./priv/ssl/server.crt -o -a ./priv/ssl/server.key ]
 then
 
 echo
-echo "priv/ssl/cert.pem and/or priv/ssl/key.pem already exist."
-echo "Delete priv/ssl/cert.pem or priv/ssl/key.pem to create new ones."
+echo "priv/ssl/server.crt and/or priv/ssl/server.key already exist."
+echo "Delete priv/ssl/server.crt and priv/ssl/server.key to create new ones."
 echo
 
 else
 
 mkdir -p priv/ssl
-umask 077 && touch priv/ssl/key.pem priv/ssl/cert.pem
-openssl genrsa 2048 > priv/ssl/key.pem
-openssl req -new -x509 -nodes -sha1 -days 3650 -out priv/ssl/cert.pem -key priv/ssl/key.pem < blank
+umask 077 && touch priv/ssl/server.key priv/ssl/server.crt
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -keyout priv/ssl/server.key -out priv/ssl/server.crt
 echo
 echo
-echo "Done creating cert..."
+echo "Done creating key and cert..."
 echo
 
 fi
